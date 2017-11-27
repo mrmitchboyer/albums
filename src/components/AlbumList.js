d19cbab0ce4ee9d0cfdc9fb20d22faad8b6db7cb
mrmitchboyer <mitch@mitchboyer.com>
2017-11-27 18:25:56 -0500

    finish first full app

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -19,14 +19,11 @@ class AlbumList extends Component {
 
   render() {
     return (
-
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
-
+      </ScrollView>
     );
   }
-
 }
 
 export default AlbumList;
